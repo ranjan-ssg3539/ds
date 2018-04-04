8e1156bb86a88eff95a6dd94c2941ada566cf83f
Ranjan Kalita <ranjankalita@bitbucket.org>
2018-04-04 21:29:58 +0530

    New project Stack Queue

---
+++ StackQueue/src/prac/stacks/Stack.java
@@ -0,0 +1,5 @@
+package prac.stacks;
+
+public class Stack {
+
+}
---
+++ StackQueue/src/prac/stacks/StackMain.java
@@ -0,0 +1,10 @@
+package prac.stacks;
+
+public class StackMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
